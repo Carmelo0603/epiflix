5031535d095819f28c81f6f983f8c4bf6e78cfb0
Carmelo0603 <carmeloprovenzani1@gmail.com>
2026-02-06 11:22:07 +0100

    commenti stupidi tolti

--- src/components/MovieGallery.jsx
+++ src/components/MovieGallery.jsx
@@ -11,9 +11,7 @@ const chunkArray = (arr, size) => {
   for (let i = 0; i < arr.length; i += size) {
     let chunk = arr.slice(i, i + size);
 
-    // SE l'ultimo gruppo è più piccolo della dimensione richiesta...
     if (chunk.length < size && arr.length >= size) {
-      // ...prendiamo direttamente gli ultimi 'size' elementi dell'array originale
       chunk = arr.slice(arr.length - size);
     }
 
@@ -30,9 +28,8 @@ class MovieGallery extends Component {
   };
 
   fetchMovies = () => {
-    // La tua API Key già inserita
     const apiKey = "34cbe335";
-    // Usiamo l'URL con la saga passata come prop
+
     const url = `https://www.omdbapi.com/?apikey=${apiKey}&s=${this.props.saga}`;
 
     fetch(url)
@@ -45,7 +42,6 @@ class MovieGallery extends Component {
       })
       .then((data) => {
         if (data.Search) {
-          // Salviamo tutti i film (non solo 6) per far funzionare lo scorrimento
           this.setState({ movies: data.Search, isLoading: false });
         } else {
           this.setState({ isLoading: false, isError: true });
@@ -76,15 +72,12 @@ class MovieGallery extends Component {
           </div>
         )}
 
-        {/* Gestione Errore */}
         {this.state.isError && <Alert variant="danger">Impossibile caricare i film per "{this.props.title}".</Alert>}
 
-        {/* Carosello Multi-item */}
         {!this.state.isLoading && !this.state.isError && (
           <Carousel indicators={false} interval={null} className="movie-carousel">
             {movieChunks.map((chunk, index) => (
               <Carousel.Item key={index}>
-                {/* Aggiungiamo justify-content-start per allineare a sinistra come l'originale */}
                 <Row className="g-2 flex-nowrap justify-content-start mx-0">
                   {chunk.map((movie) => (
                     <Col key={movie.imdbID} xs={6} md={3} lg={2} className="px-1">
